3dba65db0291ad952712d423bb3ec6f1529888aa
vitosma05 <vitoroyale05@gmail.com>
2024-10-15 09:11:14 -0300

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.navigate("Login")}
+      />
+    </View>
+  );
+}
